9a60ae82d3c770115f5e0f87e6141a75dede3697
Briggs47 <31952998+Briggs47@users.noreply.github.com>
2019-12-20 01:55:17 -0600

    Delete WinVMdeploy.ps1

--- WinVM/WinVMdeploy.ps1
+++
@@ -1,9 +0,0 @@
-New-AzResourceGroup -Name "Compute-Services-RG" -Location "Central US"
-
-New-AzResourceGroupDeployment -ResourceGroupName "Compute-Services-RG" -TemplateFile "D:\Houston\Scripts\Morpheus Compute Services\Win\template.json" -TemplateParameterFile "D:\Houston\Scripts\Morpheus Compute Services\Win\parameters.json"
-
-
-
-
-
-
